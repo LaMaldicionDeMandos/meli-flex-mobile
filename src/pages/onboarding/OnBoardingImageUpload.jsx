5c4161cb4c0c441d76094d3eab2efd970cbe5373
marcelo <pasutmarcelo@gmail.com>
2022-08-29 15:46:00 -0300

    trabajo en sacar foto

--- src/pages/onboarding/OnBoardingImageUpload.jsx
+++ src/pages/onboarding/OnBoardingImageUpload.jsx
@@ -9,14 +9,15 @@ import {idCardOutline, cloudUploadOutline, cameraOutline} from 'ionicons/icons';
 import freStyles from '../Signup.module.scss';
 import React, {useState} from "react";
 import { FilePicker } from '@capawesome/capacitor-file-picker';
-import { Camera, CameraResultType } from '@capacitor/camera';
+import {Camera, CameraDirection, CameraResultType, CameraSource} from '@capacitor/camera';
 
 
 import { head } from 'lodash';
 import imagesService from '../../services/images.service';
 
-const OnBoardingImageUpload = ({ context, nextHandler = () => {} }) => {
+const OnBoardingImageUpload = ({ context, orientation = OnBoardingImageUpload.LANDSCAPE, nextHandler = () => {} }) => {
 	const [imageUri, setImageUri] = useState();
+	const previewStyle = orientation === OnBoardingImageUpload.PORTRAIT ? freStyles.previewPortrait : freStyles.previewLandscape;
 	const addProperty = () => {
 		const delta = {};
 		delta[context.propertyName] = context.uri;
@@ -47,7 +48,9 @@ const OnBoardingImageUpload = ({ context, nextHandler = () => {} }) => {
 		const options = {
 			quality: 90,
 			allowEditing: false,
-			resultType: CameraResultType.DataUrl
+			resultType: CameraResultType.DataUrl,
+			source: CameraSource.Camera,
+			direction: CameraDirection.Front
 		}
 		try {
 			const result = await Camera.getPhoto(options);
@@ -76,8 +79,8 @@ const OnBoardingImageUpload = ({ context, nextHandler = () => {} }) => {
 				</IonRow>
 				{imageUri ? (
 					<IonRow className="ion-justify-content-center ion-align-self-center ion-text-center animate__animated animate__fadeIn">
-						<IonCol size="12" className={freStyles.preview}>
-							<img src={imageUri} className={freStyles.preview}/>
+						<IonCol size="12" className={previewStyle}>
+							<img src={imageUri} className={previewStyle}/>
 							<p>Si la foto se ve bien podes continuar</p>
 						</IonCol>
 					</IonRow>
@@ -104,5 +107,8 @@ const OnBoardingImageUpload = ({ context, nextHandler = () => {} }) => {
 		</div>
 	);
 };
+
+OnBoardingImageUpload.PORTRAIT = 'portrait';
+OnBoardingImageUpload.LANDSCAPE = 'landscape';
 
 export default OnBoardingImageUpload;
